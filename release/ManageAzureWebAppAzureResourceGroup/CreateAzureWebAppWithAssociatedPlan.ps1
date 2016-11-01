43c3b355435b04bce049cc49d9ce5b8377e6f8e3
mathieu-benoit <mathieu-benoit@hotmail.fr>
2016-10-31 21:57:46 -0400

    Add Azure Web App deployment management project.

---
+++ release/ManageAzureWebAppAzureResourceGroup/CreateAzureWebAppWithAssociatedPlan.ps1
@@ -0,0 +1,12 @@
+﻿Param(
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+    [string] $TemplateFile = 'CreateAzureWebAppWithAssociatedPlan.json'
+)
+
+Login-AzureRmAccount
+#Select-AzureSubscription "MySubscription"
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation
+New-AzureRmResourceGroupDeployment -Name $ResourceGroupName `
+                                       -ResourceGroupName $ResourceGroupName `
+                                       -TemplateFile $TemplateFile
