391141285d45866e99abde856f9b2062ceba8162
mathieu-benoit <mathieu-benoit@hotmail.fr>
2016-11-05 23:32:27 -0400

    Add UI Tests.

---
+++ test/AspNetCoreApplication.UITests/PageLoadTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.PhantomJS;
+
+namespace AspNetCoreApplication.UITests
+{
+    [TestClass]
+    public class PageLoadTests
+    {
+        public TestContext TestContext { get; set; }
+
+        string Url
+        {
+            get
+            {
+                if (TestContext.Properties["Url"] != null)
+                {
+                    return TestContext.Properties["Url"].ToString();
+                }
+                else
+                {
+                    return "http://www.google.com/";
+                }
+            }
+        }
+
+        [TestMethod]
+        public void HomepageLoad_WithPhantomJS()
+        {
+            using (var driver = new PhantomJSDriver())
+            {
+                driver.Navigate().GoToUrl(Url);
+            }
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void HomepageLoad_WithChrome()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(Url);
+            }
+            Assert.IsTrue(true);
+        }
+    }
+}
