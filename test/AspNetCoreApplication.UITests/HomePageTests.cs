59a9f72f93504d75dc62a95d53118602035add37
mathieu-benoit <mathieu-benoit@hotmail.fr>
2017-10-30 21:11:54 -0400

    Update Selenium Tests.

---
+++ test/AspNetCoreApplication.UITests/HomePageTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AspNetCoreApplication.UITests
+{
+    [TestClass]
+    public class HomePageTests : SeleniumTests
+    {
+        [TestMethod]
+        [TestCategory("UITests")]
+        public void Load_Homepage()
+        {
+            //Arrange
+            var homepageUrl = BaseUrl;
+
+            //Act
+            Driver.Navigate().GoToUrl(homepageUrl);
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrEmpty(Driver.Title));
+            Assert.IsTrue(Driver.Title.Equals("Home Page - DemoApp"));
+            Assert.IsFalse(string.IsNullOrEmpty(Driver.Url));
+            Assert.IsTrue(Driver.Url == homepageUrl);
+        }
+    }
+}
